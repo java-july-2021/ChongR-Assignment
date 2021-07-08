5e3c9a356af563a3e9aa35a146f08e6bad3f3c26
Chong Ren <renchong1993@gmail.com>
2021-07-08 10:39:09 -0400

    String Manipulation Assignment Correction

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -2,26 +2,33 @@ public class StringManipulator {
     String trimAndConcat(String A, String B){
         return A.trim()+B.trim();
     }
-    // Integer getIndexOrNull(String word, char y){
-        /*
-        String i = String.valueOf(word.indexOf(y));
-        if(i == "-1"){
+
+    Integer getIndexOrNull(String word, char y){
+        if(word.indexOf(y) == -1){
             return null;
         }
-        */
-    //    return word.indexOf(y);
-    // }
+        return word.indexOf(y);
+    }
     
     Integer getIndexOrNull(String phrase, String word){
+        if(phrase.indexOf(word) == -1){
+            return null;
+        }
         return phrase.indexOf(word);
     }
 
-    String concatSubstring(String phrase1, int i, int j, String phrase2){
-        // char a = phrase1.charAt(i);
-        // System.out.println(a);
-        // char b = phrase1.charAt(j);
-        // System.out.println(b);
-        // return a.concat(b.concat(phrase2));
-        return phrase1.charAt(i)+phrase1.charAt(j)+phrase2;
+
+    // String concatSubstring(String phrase1, int i, int j, String phrase2){
+    //     char a = phrase1.charAt(i);
+    //     System.out.println(a);
+    //     char b = phrase1.charAt(j);
+    //     System.out.println(b);
+    //     return a.concat(b.concat(phrase2));
+    //     return phrase1.charAt(i)+phrase1.charAt(j)+phrase2;
+    // }
+
+    String concatSubstring(String substringMe, int start, int end, String concatMe) {
+        concatMe = substringMe.substring(start, end) + concatMe;
+        return concatMe;
     }
 }
--- 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
@@ -1,33 +1,33 @@
 public class StringManipulatorTesting {
     public static void main(String[] args) {
         /**Q1 */
-        // StringManipulator manipulator = new StringManipulator();
-        // String str = manipulator.trimAndConcat("    Hello     ","     World    ");
-        // System.out.println(str);
+        StringManipulator manipulator = new StringManipulator();
+        String str = manipulator.trimAndConcat("    Hello     ","     World    ");
+        System.out.println(str);
 
         /**Q2 */
         // StringManipulator manipulator = new StringManipulator();
-        // char letter = 'o';
-        // Integer a = manipulator.getIndexOrNull("Coding", letter);
-        // Integer b = manipulator.getIndexOrNull("Hello World", letter);
-        // Integer c = manipulator.getIndexOrNull("Hi", letter);
-        // System.out.println(a); // 1
-        // System.out.println(b); // 4
-        // System.out.println(c); // null
+        char letter = 'o';
+        Integer a = manipulator.getIndexOrNull("Coding", letter);
+        Integer b = manipulator.getIndexOrNull("Hello World", letter);
+        Integer c = manipulator.getIndexOrNull("Hi", letter);
+        System.out.println(a); // 1
+        System.out.println(b); // 4
+        System.out.println(c); // null
 
         /**Q3 */
         // StringManipulator manipulator = new StringManipulator();
-        // String word = "Hello";
-        // String subString = "llo";
-        // String notSubString = "world";
-        // Integer a = manipulator.getIndexOrNull(word, subString);
-        // Integer b = manipulator.getIndexOrNull(word, notSubString);
-        // System.out.println(a); // 2
-        // System.out.println(b); // null
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer a1 = manipulator.getIndexOrNull(word, subString);
+        Integer b1 = manipulator.getIndexOrNull(word, notSubString);
+        System.out.println(a1); // 2
+        System.out.println(b1); // null
 
         /**Q4 */
-        StringManipulator manipulator = new StringManipulator();
-        String word = manipulator.concatSubstring("Hello", 1, 2, "world");
-        System.out.println(word); // eworld
+        // StringManipulator manipulator = new StringManipulator();
+        String word1 = manipulator.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(word1); // eworld
     }
 }
