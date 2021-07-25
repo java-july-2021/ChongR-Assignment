52c04a80c8bbcb1bb173b71ef699a024219e59e0
Chong Ren <renchong1993@gmail.com>
2021-07-25 13:56:29 -0400

    SpringBoot Assignment03

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/03-hello-human/src/main/java/com/chongren/CodingDojo/helloHuman/helloHumanController.java
@@ -0,0 +1,22 @@
+package com.chongren.CodingDojo.helloHuman;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloHumanController {
+	@RequestMapping("/")
+	public String human(){
+		System.out.println("Hello");
+		return "Hello Human!";
+	}
+	
+	@RequestMapping("/user/{name}")
+	public String greetName(@PathVariable("name") String name, Model viewModel) {
+		viewModel.addAttribute("name", name);
+		return "index.jsp";
+	}
+
+}
